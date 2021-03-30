04505ea354f734c55a198de27d6d5ca52e493a5a
Emily Pikul <emilypikul@empiks-Mac.local>
2021-03-30 13:22:05 -0500

    initial work on FBCF class functions

--- MoorerReverb/Source/FBCF.cpp
+++ MoorerReverb/Source/FBCF.cpp
@@ -14,24 +14,68 @@
 //==============================================================================
 FBCF::FBCF()
 {
-    // In your constructor, you should add any child components, and
-    // initialise any special settings that your component needs.
-
+///////////////////////////////////////////////// CODE FROM ERIN
+//    function o = LP_FBCF(Fs,dSamples,rate,amp,g)
+//                o.md = ModDelaySamples(Fs,dSamples+amp,dSamples-amp,rate);
+//                setSampleRate(o,Fs);
+//                setDelay(o,dSamples,amp)
+//                setRate(o,rate);
+//                setFBGain(o,g);
+//
+//            end
+///////////////////////////////////////////////
 }
 
 FBCF::~FBCF()
 {
 }
 
+void FBCF::processSignal(float *signal, const int numSamples, const int c)
+{
+    for (int n=0; n<numSamples; n++){
+        float x = *signal; //get value at memory location
+        x = processSample(x,c);
+        *signal = x;
+        signal++;
+        
+    }
+}
+
+float FBCF::processSample(float x, int c){
+    
+    int delayIndex = writeIndex[c] - delaySamples;
+    
+    if (delayIndex < 0){
+        delayIndex += BUFFERSIZE;
+    }
+    
+    float d = filter.processSample(w[c][delayIndex], c);
+    
+/////////////////////////////////////////////// CODE FROM ERIN
+    o.sample = o.md.processSample(x + o.g * (0.5 * o.sample + 0.5 * o.fbLPF));
+/////////////////////////////////////////////////////
+    float y = x.md.processSample(x + x.g * (0.5f * x.sample + 0.5f * o.fbLPF));
+    
+    float y = x + 0.7f * d;
+    
+    // Feed-forward
+    //w[c][index[c]] = x;
+    
+    // Feed-back
+    w[c][writeIndex[c]] = y;
+    
+    writeIndex[c]++;
+    if (writeIndex[c] >= BUFFERSIZE){
+        writeIndex[c] = 0;
+    }
+    
+    return y;
+    
+}
+
+
 void FBCF::paint (juce::Graphics& g)
 {
-    /* This demo code just fills the component's background and
-       draws some placeholder text to get you started.
-
-       You should replace everything in this method with your own
-       drawing code..
-    */
-
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));   // clear the background
 
     g.setColour (juce::Colours::grey);
@@ -45,7 +89,4 @@ void FBCF::paint (juce::Graphics& g)
 
 void FBCF::resized()
 {
-    // This method is where you should set the bounds of any child
-    // components that your component contains..
-
 }
