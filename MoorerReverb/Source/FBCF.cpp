552d6ceeabfae3b419438e651c26f59c117d63b4
Emily Pikul <emilypikul@empiks-Mac.local>
2021-03-31 11:23:07 -0500

    latest code in FBCF incorporating fractional delay class

--- MoorerReverb/Source/FBCF.cpp
+++ MoorerReverb/Source/FBCF.cpp
@@ -12,81 +12,43 @@
 #include "FBCF.h"
 
 //==============================================================================
-FBCF::FBCF()
-{
-///////////////////////////////////////////////// CODE FROM ERIN
-//    function o = LP_FBCF(Fs,dSamples,rate,amp,g)
-//                o.md = ModDelaySamples(Fs,dSamples+amp,dSamples-amp,rate);
-//                setSampleRate(o,Fs);
-//                setDelay(o,dSamples,amp)
-//                setRate(o,rate);
-//                setFBGain(o,g);
-//
-//            end
-///////////////////////////////////////////////
-}
+FBCF::FBCF(){}
 
 FBCF::~FBCF()
 {
 }
 
-void FBCF::processSignal(float *signal, const int numSamples, const int c)
-{
-    for (int n=0; n<numSamples; n++){
-        float x = *signal; //get value at memory location
-        x = processSample(x,c);
-        *signal = x;
-        signal++;
-        
-    }
+void FBCF::setGain(float newGain){
+    gain=newGain;
+     
 }
 
-float FBCF::processSample(float x, int c){
-    
-    int delayIndex = writeIndex[c] - delaySamples;
-    
-    if (delayIndex < 0){
-        delayIndex += BUFFERSIZE;
-    }
-    
-    float d = filter.processSample(w[c][delayIndex], c);
-    
-/////////////////////////////////////////////// CODE FROM ERIN
-    o.sample = o.md.processSample(x + o.g * (0.5 * o.sample + 0.5 * o.fbLPF));
-/////////////////////////////////////////////////////
-    float y = x.md.processSample(x + x.g * (0.5f * x.sample + 0.5f * o.fbLPF));
-    
-    float y = x + 0.7f * d;
-    
-    // Feed-forward
-    //w[c][index[c]] = x;
-    
-    // Feed-back
-    w[c][writeIndex[c]] = y;
-    
-    writeIndex[c]++;
-    if (writeIndex[c] >= BUFFERSIZE){
-        writeIndex[c] = 0;
-    }
+void FBCF::setFs(float newFs){
+    Fs=newFs;
+    frac.setFs(Fs);
+}
+
+void FBCF::setDelaySamples(float newDelay){
+    delay=newDelay;
+    frac.setDelaySamples(delay);
+}
+
+void FBCF::setSpeed(float newSpeed){
+    speed=newSpeed;
+    frac.setSpeed(speed);
+}
+
+void FBCF::setDepth(float newDepth){
+    depth=newDepth;
+    frac.setDepth(depth);
+}
+
+
+float FBCF::processSample(float x, int channel){
+    float w=frac.processSample(x,channel);
+    float fb= x + -gain * w;
+    float y=fb;
     
     return y;
     
-}
-
-
-void FBCF::paint (juce::Graphics& g)
-{
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));   // clear the background
-
-    g.setColour (juce::Colours::grey);
-    g.drawRect (getLocalBounds(), 1);   // draw an outline around the component
-
-    g.setColour (juce::Colours::white);
-    g.setFont (14.0f);
-    g.drawText ("FBCF", getLocalBounds(),
-                juce::Justification::centred, true);   // draw some placeholder text
-}
-
-void FBCF::resized()
-{
 }
--- MoorerReverb/Source/FBCF.h
+++ MoorerReverb/Source/FBCF.h
@@ -11,7 +11,7 @@
 #pragma once
 
 #include <JuceHeader.h>
-
+#include "FractionalDelay.hpp"
 //==============================================================================
 /*
 */
@@ -19,11 +19,30 @@ class FBCF  : public juce::Component
 {
 public:
     FBCF();
+    
     ~FBCF() override;
 
-    void paint (juce::Graphics&) override;
-    void resized() override;
+    float processSample(float x, int channel);
+    
+    void setGain(float newGain);
+    void setFs(float newFs);
+    void setDelaySamples(float newDelay);
+    void setSpeed(float newSpeed);
+    void setDepth(float newDepth);
+
+
 
 private:
+    //create instance of fractional delay in private
+    FractionalDelay frac;
+    
+    float gain =-0.7f;
+    int Fs =  48000.f;
+    float delay = 0.f;
+    float speed =0.f;
+    float depth = 5.f;
+    
+    float fb = 0.f;
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FBCF)
 };
